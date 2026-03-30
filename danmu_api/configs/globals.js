1bb9208f189d61dbff63892b6f226f90e8062c78
haode <1730142004@qq.com>
2026-03-30 20:25:12 +0800

    Add files via upload
    动漫标题缓存最大数量可配置化修改，在某些场景下通过接口获取的标题数会超过100，没有缓存到的标题无法通过id获取到弹幕，增加动漫标题缓存最大数量可配置化修改

--- danmu_api/configs/globals.js
+++ danmu_api/configs/globals.js
@@ -14,9 +14,8 @@ export const Globals = {
 
   // 静态常量
   VERSION: '1.18.0',
-  MAX_LOGS: 1000, // 日志存储，最多保存 1000 行
-  MAX_ANIMES: 100,
-  MAX_RECORDS: 100, // 请求记录最大数量
+  MAX_LOGS: 1000,
+  MAX_RECORDS: 100,
 
   // 运行时状态
   animes: [],
@@ -164,7 +163,7 @@ export const Globals = {
         // 映射大写常量到小写
         if (prop === 'version') return self.VERSION;
         if (prop === 'maxLogs') return self.MAX_LOGS;
-        if (prop === 'maxAnimes') return self.MAX_ANIMES;
+        if (prop === 'maxAnimes') return self.envs.MAX_ANIMES;
         if (prop === 'maxRecords') return self.MAX_RECORDS;
         if (prop === 'maxLastSelectMap') return self.MAX_LAST_SELECT_MAP;
 
